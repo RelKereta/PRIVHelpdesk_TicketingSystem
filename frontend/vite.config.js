db12cd0bf4da151c812be98a9f1b2b567d290532
marputtt <marsyaputra22@gmail.com>
2025-06-03 00:32:56 +0700

    deploy cicd pipeline
    Signed-off-by: marputtt <marsyaputra22@gmail.com>

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
       proxy: {
         "/api": {
           target: mode === "development"
-            ? "http://localhost:5173"
+            ? "http://localhost:5000"
             : "e2425-wads-l4acg7-server.csbihub.id",
           changeOrigin: true,
           secure: mode !== "development",
