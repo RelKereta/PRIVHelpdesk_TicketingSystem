f152c8782ec612bda7bd836644c7a78b5a3e1cb1
marputtt <marsyaputra22@gmail.com>
2025-06-02 21:23:50 +0700

    deploy cicd pipeline
    Signed-off-by: marputtt <marsyaputra22@gmail.com>

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: mode === "development"
+            ? "http://localhost:5000"
+            : "e2425-wads-l4acg7-server.csbihub.id",
+          changeOrigin: true,
+          secure: mode !== "development",
+          ws: true,
+        },
+      },
+    },
+    build: {
+      outDir: "dist",
+    },
+    plugins: [react(), tailwindcss()],
+  }
 })
