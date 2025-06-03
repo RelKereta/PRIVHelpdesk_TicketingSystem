84396607cdf2678c08ea88e16457826fbfc0fd56
marputtt <marsyaputra22@gmail.com>
2025-06-03 09:53:21 +0700

    deploy cicd pipeline
    Signed-off-by: marputtt <marsyaputra22@gmail.com>

--- backend/server/connect.js
+++ backend/server/connect.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const connectToDatabase = async () => {
     try {
-        const uri = process.env.MONGODB_URI;
+        const uri = process.env.MONGO_URI;
         if (!uri) {
             throw new Error('MONGODB_URI is not defined in environment variables');
         }
